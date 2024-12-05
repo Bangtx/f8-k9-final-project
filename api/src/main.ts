b4d89f8d91c708f06880fcdfb717af781e70b885
bangtx <bangtx@otani-trading.com>
2024-12-05 22:14:30 +0700

    added material

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,27 +3,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
 import {json} from "express";
-import {sign} from 'jsonwebtoken'
-
-
-
-const ACCESS_TOKEN_EXPIRE = 1
-const SECRET_KEY = '12bdi12ebd2d23rf2ef12'
-const ALGORITHM = 'HS256'
-
-const payload = {
-  name: 'test',
-  role: 'admin',
-  phone: '123'
-}
-
-const token = sign(payload, SECRET_KEY, { algorithm: 'RS256' }, () => {});
-console.log('token', token)
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:5000',
+    methods: ['OPTION', 'GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['*'],
+    credentials: true,
+  });
   app.use(json({ limit: '50mb' }));
 
   const config = new DocumentBuilder()
